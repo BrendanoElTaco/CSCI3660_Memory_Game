0e23084abedc931fbb1b598ed5ce6299680d04f7
BrendanoElTaco <33934145+BrendanoElTaco@users.noreply.github.com>
2024-04-22 22:20:31 -0400

    Added hyperlinks to our github pages

--- app/src/main/java/com/termproject/memorygame/HelpFragment.java
+++ app/src/main/java/com/termproject/memorygame/HelpFragment.java
@@ -2,11 +2,13 @@ package com.termproject.memorygame;
 
 import android.os.Bundle;
 import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class HelpFragment extends Fragment {
@@ -14,10 +16,11 @@ public class HelpFragment extends Fragment {
     private TextView rulesTextView;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_help, container, false);
         rulesTextView = view.findViewById(R.id.rules_text_view);
 
+        // Set the game rules with HTML formatting for bold text and hyperlinks
         String gameRules = "<b>Game Rules:</b><br><br>" +
                 "1. The game starts with all cards face down.<br>" +
                 "2. Flip over two cards.<br>" +
@@ -28,15 +31,18 @@ public class HelpFragment extends Fragment {
                 "<b>About:</b><br><br>" +
                 "This app was made as a term project for the University of North Georgia's Mobile Application Development class.<br><br>" +
                 "<b>Credits:</b><br><br>" +
-                "Brendan LeGrand<br>" +
-                "Nathan Lee<br>" +
-                "Jamie Gibbs";
+                "<a href=\"https://github.com/BrendanoElTaco\">Brendan LeGrand </a><br>" +
+                "<a href=\"https://github.com/natemvm\">Nathan Lee </a><br>" +
+                "<a href=\"https://github.com/JamieGibbs1\">Jamie Gibbs </a><br>";
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
             rulesTextView.setText(Html.fromHtml(gameRules, Html.FROM_HTML_MODE_LEGACY));
         } else {
             rulesTextView.setText(Html.fromHtml(gameRules));
         }
+
+        // Make the hyperlink clickable
+        rulesTextView.setMovementMethod(LinkMovementMethod.getInstance());
 
         return view;
     }
